574e337e79a7858a2ef1a35bd90b7a3088d1884d
akash-ranjan8 <56363385+akash-ranjan8@users.noreply.github.com>
2020-08-15 01:26:43 +0530

    Add files via upload

---
+++ double_circular_linked_list_delete_beginning.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{ int data;
+  struct node *next;
+  struct node *prev;
+}*head;
+void create(int n)
+{ int e,k,i;
+  struct node *new,*prev1;
+  printf("enter the data for 1st node\n");
+  scanf("%d",&e);
+  head=(struct node*)malloc(sizeof(struct node));
+  head->data=e;
+  head->next=NULL;
+  head->prev=NULL;
+  prev1=head;
+  for(i=2;i<=n;++i)
+  { printf("enter the data\n");
+    scanf("%d",&k);
+    new=(struct node*)malloc(sizeof(struct node));
+    new->data=k;
+    new->next=NULL;
+    new->prev=NULL;
+    prev1->next=new;
+    new->prev=prev1;
+    prev1=new;
+  }
+  prev1->next=head;
+  head->prev=prev1;
+}
+void display()
+{ struct node *ptr=head;
+  do
+  { printf("%d\t",ptr->data);
+    ptr=ptr->next;
+  }while(ptr!=head);
+}
+void delete_beginning()
+{ struct node *t=head;
+  struct node *ptr=head;
+  while(ptr->next!=head)
+  { ptr=ptr->next;
+  }
+  head=head->next;
+  head->prev=ptr;
+  ptr->next=head;
+  free(t);
+}
+int main()
+{ int n;
+  printf("enter the no. of nodes\n");
+  scanf("%d",&n);
+  create(n);
+  display();
+  printf("\n");
+  delete_beginning();
+  display();
+  return 0;
+}
